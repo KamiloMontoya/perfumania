180f293f437416a72c7a087354e5c6b2335115af
Camilo Montoya <kamilo2428@gmail.com>
2016-04-01 23:03:22 -0500

    Creación de Tips en el backend

---
+++ app/views/backend/tips/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @tip, :id, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   
+  resources :tips
   root 'frontend/welcome#index'
   #devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => ''}
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
     resources :questions 
     resources :categories
     resources :notes
+    resources :tips
 
     resources :questions do
       resources :answers
