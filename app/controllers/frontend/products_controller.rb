c62a9f0b7f7953e0a6d8c893f3d155184efea504
Camilo Montoya <kamilo2428@gmail.com>
2016-04-08 23:26:07 -0500

    paginador de productos

--- app/controllers/frontend/products_controller.rb
+++ app/controllers/frontend/products_controller.rb
@@ -6,7 +6,7 @@ class Frontend::ProductsController < Frontend::ApplicationController
   	# GET /products/1.json
 	def show
 		@category = params[:id].to_i
-		@products = Product.where(category_id: @category).order(top_position: :asc)
+		@products = Product.where(category_id: @category).order(top_position: :asc).page(params[:page]).per(10)
 	end
 
 	def show_all
