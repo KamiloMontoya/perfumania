2b94ca0dde8ca8b531d0959520ee7b46fa9fbc08
Camilo Montoya <kamilo2428@gmail.com>
2016-04-08 23:14:18 -0500

    Boston de subir

--- public/site/js/common-scripts.js
+++ public/site/js/common-scripts.js
@@ -21,23 +21,16 @@ var Script = function () {
 
 }();
 
-	(function() {
+(function() {
 
-   			$('<i id="back-to-top"></i>').appendTo($('body'));
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('#back-to-top').fadeIn();	
+		} else {
+			$('#back-to-top').fadeOut();
+		}
 
-			$(window).scroll(function() {
+	});
 
-				if($(this).scrollTop() != 0) {
-					$('#back-to-top').fadeIn();	
-				} else {
-					$('#back-to-top').fadeOut();
-				}
-
-			});
-			
-			$('#back-to-top').click(function() {
-				$('body,html').animate({scrollTop:0},600);
-			});	
-
-	})();
+})();
 
