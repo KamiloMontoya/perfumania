3f57bcb51d3193358343f9c18242f0cf7d9d3691
Camilo Montoya <kamilo2428@gmail.com>
2016-04-23 12:46:33 -0500

    Prueba configuración mailer

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,12 @@ module Perfumania
     ActionMailer::Base.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :authentication => :plain,
-      :domain => 'gmail.com',
-      :user_name => ENV['email'],
-      :password => ENV['email_password'],
+      #:authentication => :plain,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      :domain => "gmail.com",
+      :user_name => ENV['EMAIL'],
+      :password => ENV['EMAIL_PASSWORD'],
     }
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # # Default Mailer Host
+  # Rails.application.routes.default_url_options[:host] = 'domain.com'
+
+  config.action_mailer.default_url_options = { :host => 'fast-mesa-56641.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com",####important
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['EMAIL'],
+    password: ENV['EMAIL_PASSWORD']
+  }
 end
